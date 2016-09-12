54836bb458f96aeb04944c5321594c2bae738051
Damian Krzeminski <pirxpilot@code42day.com>
2016-09-12 19:41:43 -0400

    implement simple naive encoding for coordinates
    convert number from -180, 180 range into 5 digit
    representation in the 36-radix

--- test/enc36.js
+++ test/enc36.js
@@ -1,9 +1,41 @@
-var should = require('should');
 var enc36 = require('../');
 
-describe('enc36 node module', function () {
-  it('must have at least one test', function () {
-    enc36();
-    should.fail('Need to write tests.');
+var encode = enc36.encode;
+var decode = enc36.decode;
+
+
+var data = [
+  -118.4799, 33.7973,
+  -113.5231, 37.0159,
+  113.9094, -37.0159,
+  113.4587, -87.0803,
+  0, 0,
+  -180, 180
+];
+
+var str = '3nux6cq8pe3yhe2cx51yhhy708igq2hgzf25j5qaapsw0apsw000000lfls0';
+
+describe('enc36', function () {
+  it('must encode empty Array', function () {
+    encode([]).should.eql('');
   });
+
+  it('must decode empty String', function () {
+    decode('').should.eql([]);
+  });
+
+  it('must encode Array of numbers', function () {
+    encode(data).should.eql(str);
+  });
+
+  it('must decode String', function () {
+    var d = decode(str);
+
+    d.should.have.length(data.length);
+
+    d.forEach(function(elem, i) {
+      elem.should.approximately(data[i], 0.000001);
+    });
+  });
+
 });
