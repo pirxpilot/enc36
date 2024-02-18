b36af095305f3dadfa2eb191787f0cbad01742a6
Damian Krzeminski <pirxpilot@furkot.com>
2024-02-18 16:17:21 +0100

    modernize sources

--- test/enc36.js
+++ test/enc36.js
@@ -1,7 +1,7 @@
 const { describe, it } = require('node:test');
 const { encode, decode } = require('../');
 
-var data = [
+const data = [
   -118.4799, 33.7973,
   -113.5231, 37.0159,
   113.9094, -37.0159,
@@ -10,7 +10,7 @@ var data = [
   -180, 180
 ];
 
-var str = '3nux6cq8pe3yhe2cx51yhhy708igq2hgzf25j5qaapsw0apsw000000lfls0';
+const str = '3nux6cq8pe3yhe2cx51yhhy708igq2hgzf25j5qaapsw0apsw000000lfls0';
 
 describe('enc36', function () {
   it('must encode empty Array', function () {
@@ -26,11 +26,11 @@ describe('enc36', function () {
   });
 
   it('must decode String', function () {
-    var d = decode(str);
+    const d = decode(str);
 
     d.should.have.length(data.length);
 
-    d.forEach(function(elem, i) {
+    d.forEach(function (elem, i) {
       elem.should.approximately(data[i], 0.000001);
     });
   });
